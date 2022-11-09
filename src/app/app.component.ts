be471ba9e40e430ad1df152cfa84edbac85b02a2
theGeekkit <sonofgondolin16@gmail.com>
2022-11-09 17:19:46 -0600

    11-9-22
    Co-authored-by: jLamwuzhere <jLamwuzhere@users.noreply.github.com>
    Co-authored-by: chrisHembree <chrisHembree@users.noreply.github.com>
    Co-authored-by: kylesteinmann <kylesteinmann@users.noreply.github.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,20 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 export class AppComponent {
   title = 'cat-facts';
   fact: any= {}
+  pic: any= {}
 
   constructor(private http: HttpClient) {
     this.getRandomFact()
+    this.getRandomPic()
   }
 
   getRandomFact() {
     this.http.get('https://meowfacts.herokuapp.com?count=1').subscribe((result: any) => {
       this.fact = result.data[0]})
+  }
+
+  getRandomPic() {
+    this.http.get('https://api.thecatapi.com/v1/images/search?limit=1').subscribe((result: any)=> {
+      this.pic = result.data[0]})
   }
 }
